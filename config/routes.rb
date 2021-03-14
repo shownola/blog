02151b8f6a56f05675c429c7ff34beb559327121
shownola <swasieko@gmail.com>
2021-03-14 13:44:44 -0700

    Add article validations, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'about', to: 'pages#about'
-
   root 'pages#home'
+  resources :articles
 
 
 end
