118267983760301fe2983394a67a30d650f9bfb1
shownola <swasieko@gmail.com>
2021-03-15 09:11:36 -0700

    Refactor views and complete crud actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
     @articles = Article.all
@@ -6,6 +7,50 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Your article was created successfully'
+      redirect_to @article
+    else
+      flash[:error] = 'There was an error, please try again'
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article has been updated"
+      redirect_to @article
+    else
+      flash[:error] = 'There was an error, please try again!'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 3, maximum: 150}
   validates :description, presence: true, length: {minimum: 3, maximum: 2500}
 
+
 end
