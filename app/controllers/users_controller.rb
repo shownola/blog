2befdae0cd6508038f334c24e2a0a369a3988b97
shownola <swasieko@gmail.com>
2021-03-16 14:55:06 -0700

    Add User resources and created obj to share errors between users and articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:username)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'about', to: 'pages#about'
   root 'pages#home'
   resources :articles
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
 
 
 end
