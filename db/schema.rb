91e843432e240d2c48d4ce7bd54b94e6570ff36d
shownola <swasieko@gmail.com>
2021-03-16 11:17:05 -0700

    Add email to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_174435) do
+ActiveRecord::Schema.define(version: 2021_03_16_180951) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_174435) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
