3c78932f86a32265d05ff48b85f5464b2ba34bb5
shownola <swasieko@gmail.com>
2021-03-16 11:06:17 -0700

    Add user models and associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,7 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 3, maximum: 150}
   validates :description, presence: true, length: {minimum: 3, maximum: 2500}
 
+  belongs_to :user
+
 
 end
