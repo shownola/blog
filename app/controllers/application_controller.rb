5aa5f808f85cc25394c4b05fdaf466891dc6ca48
shownola <swasieko@gmail.com>
2021-03-16 06:56:17 -0700

    Add styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = ShownolaViewTool::Renderer.copyright 'Shownola | Sherry Wasieko', 'All rights reserved'
+  end
+end
+
+module ShownolaViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b> #{name}</b> #{msg}" .html_safe
+    end
+  end
 end
