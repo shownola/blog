80b16c4ed2a94d5c67e7c2de88dfde15712db61f
shownola <swasieko@gmail.com>
2021-03-17 09:49:00 -0700

    Part 1 of creating login sessions

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   resources :articles
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 
 
 end
