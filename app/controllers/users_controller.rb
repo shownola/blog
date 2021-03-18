5d7a6738bf97aac4e2e43b7b1f445c41474c6ac9
shownola <swasieko@gmail.com>
2021-03-18 08:49:00 -0700

    Set permissions for admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @user
+      if current_user != @user && !current_user.admin?
         flash[:alert] = 'You can only edit your own profile'
         redirect_to @user
       end
