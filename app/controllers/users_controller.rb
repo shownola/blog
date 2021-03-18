0377d6497b13bc40a8eb852f15d46130bf95c832
shownola <swasieko@gmail.com>
2021-03-18 08:05:46 -0700

    Add admin to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy]
-  before_action :require_user, except: %i[show index]
-  before_action :require_same_user, only: %i[edit update]
+  before_action :require_user, only: %i[edit update]
+  before_action :require_same_user, only: %i[edit update destroy]
 
 
   def index
@@ -19,18 +19,6 @@ class UsersController < ApplicationController
   def edit
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:notice] = "Welcome #{@user.username} You have successfully signed up"
-      redirect_to @user
-    else
-      flash[:error] = "There was an error, please try again"
-      render 'new'
-    end
-  end
-
   def update
       if @user.update(user_params)
         flash[:notice] = "User was successfully updated."
@@ -39,6 +27,18 @@ class UsersController < ApplicationController
         flash[:error] = 'There was an error, please try again!'
         render 'edit'
       end
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome #{@user.username} You have successfully signed up"
+      redirect_to articles_path
+    else
+      flash[:error] = "There was an error, please try again"
+      render 'new'
+    end
   end
 
   def destroy
