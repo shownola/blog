dd51c8a5d56e9646926671dff1df8448d0069829
shownola <swasieko@gmail.com>
2021-03-19 07:12:54 -0700

    Add associations in the UI

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def create
+  def create    
     @article = Article.new(article_params)
     #@article.user = User.first
     @article.user = current_user
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
